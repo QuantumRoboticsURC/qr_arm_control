6d66fd33738f2ba04ecdbe17c618bd29f78fc3fd
iker <A01749675@tec.mx>
2023-02-16 16:53:16 -0600

    Update

--- scripts/joystick.py
+++ scripts/joystick.py
@@ -2,7 +2,7 @@
 import rospy
 import rospkg
 from geometry_msgs.msg import Twist
-from Tkinter import *
+#from Tkinter import *
 from PIL import ImageTk, Image
 import time
 import os
@@ -15,6 +15,8 @@ import math
 import numpy
 import cmath
 from sensor_msgs.msg import Joy
+import sys
+
 
 
 values_map={   
@@ -22,8 +24,8 @@ values_map={
     "y": 0,      
     "z": 0,
     "phi": 0,
-    "rotation": 0,
-    "joint8": 0
+    "joint5":0,
+    "joint8":0
 }
 
 def change_value(arr, index, val):
@@ -33,6 +35,14 @@ def change_value(arr, index, val):
         return val*-1
     else:
         return 0
+def change_value2(arr, indexb,indexa, val):
+    if buttons[indexb] == 1.0:
+        return val
+    elif axes[indexa] == -1.0:
+        return val*-1
+    else:
+        return 0
+
 
 def triggers(val):
     global axes
@@ -43,17 +53,17 @@ def triggers(val):
     return 0
     
         
-buttons, axes = [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0]
+buttons, axes = [0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0]
 def to_string():
     print(values_map)
     return str(values_map["x"]) + " " + str(values_map["y"]) + \
-    " " + str(values_map["z"]) + " " + str(values_map["phi"]) + \
-    " " + str(values_map["rotation"]) + " " + str(values_map["joint8"])
+    " " + str(values_map["z"]) + " " + str(values_map["phi"]) +" "+str(values_map["joint5"])+" "+str(values_map["joint8"])
 
 def on_joy(data):
     global buttons, axes
     buttons = list(data.buttons [:])
     axes = list(data.axes [:])
+   
     if buttons[0] == 1:
         pub_predefined.publish("FLOOR")
     elif buttons[1] == 1:
@@ -61,23 +71,39 @@ def on_joy(data):
     elif buttons[2] == 1:
         pub_predefined.publish("INTERMEDIATE")
 
+def servo_mover(b):
+    if axes[b] == 1.0:
+        return 1.0
+    elif axes[b]==-1:
+        return -1.0
+    else:
+        return 0
+
+def pause():
+    global buttons
+    if(buttons[9]==1):
+        sys.exit()
 
 rospy.init_node("arm_joystick")
 rospy.Subscriber("joy",Joy,on_joy)
 pub = rospy.Publisher('goal', String, queue_size=1)
 pub_predefined = rospy.Publisher('predefined', String, queue_size=1)
 pub_prism = rospy.Publisher('arm_teleop/prism', Float64, queue_size=1)
+
 last_prism = 0
 
 rate = rospy.Rate(20)
 
 while not rospy.is_shutdown():
+    pause()
     changed = False
-    values_map["x"] = triggers(.02)
+    values_map["x"] = change_value(axes,2,.02)*-1
     values_map["y"] = change_value(axes, 0, .22)*-1
     values_map["z"] = change_value(axes, 1, .05)
-    values_map["phi"] = change_value(axes, 4, .8)
-    values_map["rotation"] = change_value(axes, 3, .05)*-1
+    values_map["phi"] = change_value(axes, 5, .8)
+    #values_map["rotation"] = change_value2(axes, 4,3, .05)
+    values_map["joint5"]=servo_mover(6)
+    values_map["joint8"]=servo_mover(7)
     if axes[-1] != last_prism:
         pub_prism.publish(axes[-1])
         last_prism = axes[-1]
